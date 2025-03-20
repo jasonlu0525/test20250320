2e0452d685fe6e916080e316b7e4e92f010bd882
JasonLu <s099113302@gmail.com>
2025-03-20 13:51:53 +0800

    test error

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/test20250320/',
+  // base:'/test20250320/',
   plugins: [
     vue(),
     vueDevTools(),
