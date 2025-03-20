f83365de5305f27abd75434b260ad7e91deef04b
JasonLu <s099113302@gmail.com>
2025-03-20 13:46:38 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/test20250320/',
   plugins: [
     vue(),
     vueDevTools(),
